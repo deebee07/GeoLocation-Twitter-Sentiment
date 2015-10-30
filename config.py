c496bce94cc0b1bcd6eb0c9ee96d60d951195d61
Devashish Badlani <deebee2307@gmail.com>
2015-10-29 20:57:59 -0700

    Commit Final with Graph
    Commit Final with Graph

--- config.py
+++ config.py
@@ -4,7 +4,7 @@
 #	https://apps.twitter.com/app/new
 #--------------------------------------------------------------------------------
 
-consumer_key = 'ZTdlGtRjnkDmqAloKC6WQj04I'
-consumer_secret = 'VNlrCtUanvjuHfe2d0FL0lprBkpjJJhUF8ymtI8GpHGXN7u1TI'
-access_token = '151759274-PtMZiaaUk51HDvjzNfSLXuRPUAFHbfMNvD5oK8yG'
-access_secret = 'mCebLrLDhcBNRqlqUqdFvGSo4XYroVBUZWVXlffWaTaei'
+consumer_key = 'Rh2rVx9MFzYvlTuHFdi75bnMU'
+consumer_secret = '50cqhleacCpxVPVDrzVmwHMA9K8TPT5BQ52MpDsL9NBwGtgOPf'
+access_token = '2946417336-TyVVVgZllAeDRrWZJgXk7FKP9bHHipI6vXtmDjv'
+access_secret = '9rUNNXpX1gtgDWXfH0TJt7Gu87pPZ1jX07xbvf5cZdnx9'
--- twitter-sentiment-analysis.py
+++ twitter-sentiment-analysis.py
@@ -1,11 +1,4 @@
 #!/usr/bin/python
-
-#-----------------------------------------------------------------------
-# twitter-stream-format:
-#  - ultra-real-time stream of twitter's public timeline.
-#    does some fancy output formatting.
-#-----------------------------------------------------------------------
-
 from twitter import *
 import re
 import sys
@@ -13,7 +6,7 @@ import math
 import re
 import pandas
 
-search_term = "Chocolate"
+search_term = raw_input("What do you want to search ")
 
 #-----------------------------------------------------------------------
 # import a load of external features, for text display and date handling
@@ -25,9 +18,9 @@ from time import strftime
 from textwrap import fill
 from termcolor import colored
 from email.utils import parsedate
-
+from geopy.geocoders import Nominatim
 #-----------------------------------------------------------------------
-# load our API credentials 
+# load API credentials 
 #-----------------------------------------------------------------------
 config = {}
 execfile("config.py", config)
@@ -36,9 +29,9 @@ reload(sys)
 sys.setdefaultencoding('utf-8')
 
 # AFINN-111 is as of June 2011 the most recent version of AFINN
-filenameAFINN = 'Dict/DictSent.txt'
-afinn = dict(map(lambda (w, s): (w, int(s)), [ 
-            ws.strip().split('\t') for ws in open(filenameAFINN) ]))
+filenameDictionary = 'Dictionary.txt'
+dictionary = dict(map(lambda (w, s): (w, int(s)), [ 
+            ws.strip().split('\t') for ws in open(filenameDictionary) ]))
 
 # Word splitter pattern
 pattern_split = re.compile(r"\W+")
@@ -46,16 +39,15 @@ pattern_split = re.compile(r"\W+")
 
 
 
-def sentiment(text):
+def sentimentAnalysis(text):
     """
     Returns a float for sentiment strength based on the input text.
     Positive values are positive valence, negative value are negative valence. 
     """
     words = pattern_split.split(text.lower())
-    sentiments = map(lambda word: afinn.get(word, 0), words)
+    sentiments = map(lambda word: dictionary.get(word, 0), words)
     if sentiments:
-        # How should you weight the individual word sentiments? 
-        # You could do N, sqrt(N) or 1 for example. Here I use sqrt(N)
+        
         sentiment = float(sum(sentiments))/math.sqrt(len(sentiments))
         
     else:
@@ -74,7 +66,11 @@ stream = TwitterStream(auth = auth, secure = True)
 #-----------------------------------------------------------------------
 # iterate over tweets matching this filter text
 #-----------------------------------------------------------------------
-tweet_iter = stream.statuses.filter(track = search_term,geocode = '-81.474144, 34,100')
+input_location = raw_input("Enter the name of the city: ")
+geolocator = Nominatim()
+location = geolocator.geocode(input_location)
+geocodeStr = repr(location.latitude) + "," + repr(location.longitude)
+tweet_iter = stream.statuses.filter(track = search_term,geocode = geocodeStr)
 
 
 
@@ -110,7 +106,7 @@ for tweet in tweet_iter:
 			print "(%s) @%s" % (time_colored, user_colored)
 			print "%s" % (text_colored)
 			i=i+1
-			sentiments = map(sentiment, [ tweet['text'] ])
+			sentiments = map(sentimentAnalysis, [ tweet['text'] ])
 			Sentimentscore=sum(sentiments)/math.sqrt(len(sentiments))
 			if Sentimentscore>0.0:
 				positive=positive+1
@@ -126,8 +122,20 @@ for tweet in tweet_iter:
 		else:
 			break
 
-print "SEARCHED TERM DONALD"
-print "GEO LOCATION SAN JOSE"
-print "TOTAL TWEETS ANALYZED 100"
-print "COUNT OF POSITIVE IS "+str(positive)+ "  COUNT OF NEGATIVE IS "+str(negative) + "  COUNT OF NEUTRAL IS "+ str (neutral)
+print "TOP 100 RECENT TWEETS THAT HAD "+search_term+" WITH GEO LOCATION "+"GEO LOCATION "+ location.address 
 
+print "COUNT OF POSITIVE IS "+str(positive)+ "  COUNT OF NEGATIVE IS "+str(negative) + "  COUNT OF NEUTRAL IS "+ str (neutral)
+import matplotlib.pyplot as plt; plt.rcdefaults()
+import numpy as np
+import matplotlib.pyplot as plt
+ 
+objects = ('POSITIVE', 'NEUTRAL', 'NEGATIVE')
+y_pos = np.arange(len(objects))
+performance = [positive,neutral,negative]
+ 
+plt.bar(y_pos, performance, align='center', alpha=0.5)
+plt.xticks(y_pos, objects)
+plt.ylabel('Tweet Count')
+plt.title('Sentiment of %s about %s' %(input_location, search_term))
+ 
+plt.show()
